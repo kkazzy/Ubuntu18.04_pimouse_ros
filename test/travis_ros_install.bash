ea9a8e1ed2a01eb3200b7b7f476ebe4550a87541
Kazuyuki Kawazu <knz_soft@yahoo.co.jp>
2020-03-27 09:09:51 +0900

    modified 16 -> 18.04

--- test/travis_ros_install.bash
+++ test/travis_ros_install.bash
@@ -8,15 +8,15 @@ pip install rospkg
 
 #ros install
 cd ..
-git clone https://github.com/kkazzy/ros_setup_scripts_Ubuntu16.04_server.git
-cd ./ros_setup_scripts_Ubuntu16.04_server
+git clone https://github.com/kkazzy/ros_setup_scripts_Ubuntu18.04_desktop.git
+cd ./ros_setup_scripts_Ubuntu18.04_desktop
 bash ./step0.bash
 bash ./step1.bash
 
 #catkin_setup
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/src
-source /opt/ros/indigo/setup.bash
+source /opt/ros/melodic/setup.bash
 catkin_init_workspace
 cd ~/catkin_ws
 catkin_make
